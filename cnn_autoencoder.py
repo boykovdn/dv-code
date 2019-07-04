d9a30fac93b23c41825937c39a770bf09615aca9
dv016 <dv016@pooluser>
2019-07-04 20:41:50 +0200

    Update to batchnorm cnn

--- cnn_autoencoder.py
+++ cnn_autoencoder.py
@@ -37,16 +37,25 @@ class CNNAutoencoder(nn.Module):
                 }
 
         self.conv1 = nn.Conv2d(*self.ae_shape['conv1'], padding=1)
+        self.batchnorm1 = nn.BatchNorm2d(self.ae_shape['conv1'][1])
         self.conv2 = nn.Conv2d(*self.ae_shape['conv2'], padding=1)
+        self.batchnorm2 = nn.BatchNorm2d(self.ae_shape['conv2'][1])
         self.conv3 = nn.Conv2d(*self.ae_shape['conv3'], padding=1)
+        self.batchnorm3 = nn.BatchNorm2d(self.ae_shape['conv3'][1])
         self.conv4 = nn.Conv2d(*self.ae_shape['conv4'], padding=1)
+        self.batchnorm4 = nn.BatchNorm2d(self.ae_shape['conv4'][1])
         self.conv5 = nn.Conv2d(*self.ae_shape['conv5'], padding=1)
+        self.batchnorm5 = nn.BatchNorm2d(self.ae_shape['conv5'][1])
 
         # Decoder learnable layers
         self.upconv1 = nn.ConvTranspose2d(*self.ae_shape['upconv1'], stride=2, padding=1,output_padding=1)
+        self.batchnorm6 = nn.BatchNorm2d(self.ae_shape['upconv1'][1])
         self.upconv2 = nn.ConvTranspose2d(*self.ae_shape['upconv2'], stride=2, padding=1, output_padding=1)
+        self.batchnorm7 = nn.BatchNorm2d(self.ae_shape['upconv2'][1])
         self.upconv3 = nn.ConvTranspose2d(*self.ae_shape['upconv3'], stride=2, padding=1, output_padding=1)
+        self.batchnorm8 = nn.BatchNorm2d(self.ae_shape['upconv3'][1])
         self.upconv4 = nn.ConvTranspose2d(*self.ae_shape['upconv4'], stride=2, padding=1, output_padding=1)
+        self.batchnorm9 = nn.BatchNorm2d(self.ae_shape['upconv4'][1])
         self.upconv5 = nn.ConvTranspose2d(*self.ae_shape['upconv5'], stride=2, padding=1, output_padding=1)
 
         # Activation and pooling layers
@@ -54,16 +63,16 @@ class CNNAutoencoder(nn.Module):
         self.pool2d = nn.MaxPool2d(2)
         
     def forward(self, x):
-        out = self.pool2d(self.relu(self.conv1(x)))
-        out = self.pool2d(self.relu(self.conv2(out)))
-        out = self.pool2d(self.relu(self.conv3(out)))
-        out = self.pool2d(self.relu(self.conv4(out)))
-        out = self.pool2d(self.relu(self.conv5(out)))
+        out = self.pool2d(self.batchnorm1(self.relu(self.conv1(x))))
+        out = self.pool2d(self.batchnorm2(self.relu(self.conv2(out))))
+        out = self.pool2d(self.batchnorm3(self.relu(self.conv3(out))))
+        out = self.pool2d(self.batchnorm4(self.relu(self.conv4(out))))
+        out = self.pool2d(self.batchnorm5(self.relu(self.conv5(out))))
 
-        out = self.relu(self.upconv1(out))
-        out = self.relu(self.upconv2(out))
-        out = self.relu(self.upconv3(out))
-        out = self.relu(self.upconv4(out))
+        out = self.batchnorm6(self.relu(self.upconv1(out)))
+        out = self.batchnorm7(self.relu(self.upconv2(out)))
+        out = self.batchnorm8(self.relu(self.upconv3(out)))
+        out = self.batchnorm9(self.relu(self.upconv4(out)))
         out = self.upconv5(out)
         
         return out
@@ -101,7 +110,7 @@ def train(ae, dataloader, criterion, optimizer, use_gpu=True, epochs=5):
 if __name__ == "__main__":
     image_size = (128,128)
     data_path = "/export/home/dv/dv016/datasets/cell_images/Uninfected"
-    batch_size = 256
+    batch_size = 128
     
     ae = CNNAutoencoder()
     transforms = torchvision.transforms.Compose([ 
